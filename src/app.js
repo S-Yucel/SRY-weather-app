2b21ca05a8fb7d68ce7ee87fd534144a025813d9
sse367 <sariyeyucel@icloud.com>
2022-09-29 22:12:59 +0300

    cityElement added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "25cd738e3cff88977f45d77cbd9bfef3";
