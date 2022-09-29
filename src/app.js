b3967552aaed4f606304b6fb704f3ef6dfdca35f
sse367 <sariyeyucel@icloud.com>
2022-09-29 22:26:56 +0300

    city and description element added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data.name);
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  
+ 
+  
 }
 
 let apiKey = "25cd738e3cff88977f45d77cbd9bfef3";
