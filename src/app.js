d9f3c4850b28694f65419b673a2225372a105cb7
Sariye <sariyeyucel@icloud.com>
2022-10-07 16:31:42 +0300

    updated

--- src/app.js
+++ src/app.js
@@ -22,6 +22,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML =`<div class="row">`;
+
+  let days = ["Fri","Sat","Sun","Mon"];
+  days.forEach(function(day) {
+    forecastHTML = forecastHTML + `
+  
+    <div class="col-2">
+      <div class="weather-forecast-date">
+      ${day}
+      </div>
+      <img 
+        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+        alt="" 
+        width="46"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">
+          18°
+        </span>
+        <span class="weather-forecast-temperature-min">
+          12° 
+        </span>
+      </div>
+    </div>
+  
+`;
+
+  })
+
+
+  
+forecastHTML = forecastHTML+`</div>`
+  forecastElement.innerHTML = forecastHTML;
+
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -90,3 +129,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Istanbul");
+displayForecast();
